232926299f11fee9b4d311d9544b6c070208bedf
sunlei <1255193087@qq.com>
2023-09-04 14:40:49 +0800

    。。。

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -1,28 +1,33 @@
 <template>
   <div class="login-page">
-    <van-nav-bar
-      title="登录"
-    />
+    <van-nav-bar title="登录"/>
     <van-form @submit="onSubmit">
       <van-field
         v-model="username"
-        name="用户名"
+        name="username"
         label="用户名"
         placeholder="用户名"
-        :rules="[{ required: true, message: '请填写用户名' }]"
+        :rules="[
+          { required: true, message: '请填写用户名' },
+          { pattern: /^\w{5,}$/, message: '用户名长度至少为5个字符' },
+        ]"
       />
       <van-field
         v-model="password"
         type="password"
-        name="密码"
+        name="password"
         label="密码"
         placeholder="密码"
-        :rules="[{ required: true, message: '请填写密码' }]"
+        :rules="[
+          { required: true, message: '请填写密码' },
+          { pattern: /^\w{6,}$/, message: '密码长度至少为6个字符' },
+        ]"
       />
       <div style="margin: 16px;">
-        <van-button round block type="info" native-type="submit">提交</van-button>
+        <van-button round block type="info" native-type="submit">登录</van-button>
       </div>
-  </van-form>
+    </van-form>
+    <router-link class="link" to="/register">注册账号</router-link>
   </div>
 </template>
 
--- src/views/Register.vue
+++ src/views/Register.vue
@@ -1,6 +1,33 @@
 <template>
   <div class="register-page">
-    注册页
+    <van-nav-bar title="注册"/>
+    <van-form @submit="onSubmit">
+      <van-field
+        v-model="username"
+        name="username"
+        label="用户名"
+        placeholder="用户名"
+        :rules="[
+          { required: true, message: '请填写用户名' },
+          { pattern: /^\w{5,}$/, message: '用户名长度至少为5个字符' },
+        ]"
+      />
+      <van-field
+        v-model="password"
+        type="password"
+        name="password"
+        label="密码"
+        placeholder="密码"
+        :rules="[
+          { required: true, message: '请填写密码' },
+          { pattern: /^\w{6,}$/, message: '密码长度至少为6个字符' },
+        ]"
+      />
+      <div style="margin: 16px;">
+        <van-button round block type="info" native-type="submit">注册</van-button>
+      </div>
+    </van-form>
+    <router-link class="link" to="/login">已有账号，返回登录页</router-link>
   </div>
 </template>
 
@@ -9,11 +36,14 @@ export default {
   name: 'RegisterPage',
   data () {
     return {
-
+      username: '',
+      password: ''
     }
   },
   methods: {
-
+    onSubmit (values) {
+      console.log('submit', values)
+    }
   }
 }
 </script>
