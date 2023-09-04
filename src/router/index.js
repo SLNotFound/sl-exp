d82ed9867efa0ee094804c39dff97d1f33f2cbf1
sunlei <1255193087@qq.com>
2023-09-04 11:13:24 +0800

    引入路由&定制主题

--- src/router/index.js
+++ src/router/index.js
@@ -1,12 +1,19 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
+import Login from '@/views/Login'
+import Detail from '@/views/Detail'
+import Register from '@/views/Register'
+import Layout from '@/views/Layout'
 
 Vue.use(VueRouter)
 
-const routes = []
-
 const router = new VueRouter({
-  routes
+  routes: [
+    { path: '/login', component: Login },
+    { path: '/detail/:id', component: Detail },
+    { path: '/register', component: Register },
+    { path: '/layout', component: Layout }
+  ]
 })
 
 export default router
---
+++ src/views/Layout.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="layout-page">
+    <div>首页架子 - 内容区域</div>
+
+    <van-tabbar v-model="active">
+      <van-tabbar-item icon="label-o">面经</van-tabbar-item>
+      <van-tabbar-item icon="star-o">收藏</van-tabbar-item>
+      <van-tabbar-item icon="like-o">喜欢</van-tabbar-item>
+      <van-tabbar-item icon="manager-o">我的</van-tabbar-item>
+    </van-tabbar>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'LayoutPage'
+}
+</script>
+
+<style lang="less" scoped></style>
