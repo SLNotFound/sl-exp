853d69aec51787449cbc494b4011751b7051552f
sunlei <1255193087@qq.com>
2023-09-04 11:43:43 +0800

    登录页

--- src/utils/vant-ui.js
+++ src/utils/vant-ui.js
@@ -1,6 +1,9 @@
 import Vue from 'vue'
-import { Button, Tabbar, TabbarItem } from 'vant'
+import { Button, Tabbar, TabbarItem, Form, Field, NavBar } from 'vant'
 
 Vue.use(Button)
 Vue.use(Tabbar)
 Vue.use(TabbarItem)
+Vue.use(Form)
+Vue.use(Field)
+Vue.use(NavBar)
--- src/views/Login.vue
+++ src/views/Login.vue
@@ -1,6 +1,28 @@
 <template>
   <div class="login-page">
-    登录页
+    <van-nav-bar
+      title="登录"
+    />
+    <van-form @submit="onSubmit">
+      <van-field
+        v-model="username"
+        name="用户名"
+        label="用户名"
+        placeholder="用户名"
+        :rules="[{ required: true, message: '请填写用户名' }]"
+      />
+      <van-field
+        v-model="password"
+        type="password"
+        name="密码"
+        label="密码"
+        placeholder="密码"
+        :rules="[{ required: true, message: '请填写密码' }]"
+      />
+      <div style="margin: 16px;">
+        <van-button round block type="info" native-type="submit">提交</van-button>
+      </div>
+  </van-form>
   </div>
 </template>
 
@@ -8,10 +30,15 @@
 export default {
   name: 'LoginPage',
   data () {
-    return {}
+    return {
+      username: '',
+      password: ''
+    }
   },
   methods: {
-
+    onSubmit (values) {
+      console.log('submit', values)
+    }
   }
 }
 </script>
