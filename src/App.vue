4891c8ac64f86a0061db1415f4f30850d8328d51
sunlei <1255193087@qq.com>
2023-09-04 10:35:15 +0800

    引入postcss

--- src/App.vue
+++ src/App.vue
@@ -5,9 +5,15 @@
     <van-button type="default">默认按钮</van-button>
     <van-button type="warning">警告按钮</van-button>
     <van-button type="danger">危险按钮</van-button>
+    <div class="box"></div>
     <router-view/>
   </div>
 </template>
 
 <style lang="less">
+.box {
+  width: 200px;
+  height: 200px;
+  background-color: pink;
+}
 </style>
